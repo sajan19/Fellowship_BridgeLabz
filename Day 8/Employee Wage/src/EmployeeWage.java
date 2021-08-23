a8c635162658ba2771543040eb94705839ea1471
Sajan <sajanmhatre29@gmail.com>
2021-08-24 01:57:59 +0530

    Attendance of Employee's

--- Day 8/Employee Wage/src/EmployeeWage.java
+++ Day 8/Employee Wage/src/EmployeeWage.java
@@ -1,5 +1,23 @@
+import java.util.Random;
+
 public class EmployeeWage {
+    public void Attendance() {
+        System.out.println("Attendance of Employees: ");
+        double isPresent=Math.floor(Math.random()*10)%2;
+        System.out.println(isPresent);
+        if(isPresent==1){
+            System.out.println("Employee is Present");
+        }
+        else{
+            System.out.println("Employee is Absent");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+        System.out.println("Welcome to Employee Wage Computation Program");
+        //Object
+        EmployeeWage obj = new EmployeeWage();
+        obj.Attendance();
+
     }
 }
